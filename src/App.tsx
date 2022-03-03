4cd733ab49c16e957fb6af01eded3d7aa6de708f
mazurenka <an.drej@mail.ru>
2022-03-03 15:06:11 +0100

    11

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {Header} from './Header';
+import {Body} from './Body';
+import {Footer} from "./Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [students, setStudents] = useState([
+        {id: 1, name: 'James', age: 8},
+        {id: 2, name: 'James', age: 28},
+        {id: 3, name: 'James', age: 38},
+        {id: 4, name: 'James', age: 48},
+        {id: 5, name: 'James', age: 58},
+    ])
+
+    return (
+        <div className="App">
+            <Header titleForHeader={'New Header'}/>
+            <Body titleForBody={'New Body'}/>
+            <Footer titleForFooter={'New Footer'}/>
+
+        </div>
+    );
 }
 
 export default App;
